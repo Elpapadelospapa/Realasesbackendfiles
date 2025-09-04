0e91e56daba52071984c3d9cf5110eb08e239125
Elpapadelospapa <quijadaalfredo723@gmail.com>
2025-09-03 21:38:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,10 @@ async def enviar_mensaje(mensaje: dict):
 async def root():
 
     return {"mensaje": "Backend funcionando en Railway"}
+    
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
